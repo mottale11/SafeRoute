08073463340a9a1175ffb853db86a29805ddbef3
mottale11 <mosesmwai100@gmail.com>
2025-12-10 12:35:24 +0300

    Deployment

--- saferoute/settings.py
+++ saferoute/settings.py
@@ -136,8 +136,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = [os.path.join(BASE_DIR, 'static')]
 
 # Media files
 MEDIA_URL = 'media/'
